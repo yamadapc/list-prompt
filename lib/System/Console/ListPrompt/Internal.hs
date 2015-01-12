3ee0c827eb95b29de2f28727ea86e506e45d43f6
yamadapc <tacla.yamada@gmail.com>
2015-01-12 16:03:41 -0200

    Copy a couple of useful functions from questioner
    These are bracket protected terminal handling helpers.

--- lib/System/Console/ListPrompt/Internal.hs
+++ lib/System/Console/ListPrompt/Internal.hs
@@ -2,7 +2,10 @@ module System.Console.ListPrompt.Internal
   where
 
 import Control.Applicative ((<$>))
+import Control.Exception (bracket_)
+import System.Console.ANSI
 import System.Console.Terminal.Size (size, Window(..))
+import System.IO (hGetBuffering, hSetBuffering, hSetEcho, stdin)
 
 import System.Console.ListPrompt.Types
 
@@ -11,11 +14,24 @@ import System.Console.ListPrompt.Types
 -- terminal's size and -- the number of available choices
 getDimensions :: Int -> Maybe (Window Int) -> ListPromptDimensions
 getDimensions numChoices (Just (Window h w)) =
-    ListPromptDimensions ((h `div` 2 - numChoices `div` 2) + padding, padding)
-                         (numChoices + margin, w - margin)
+    ListPromptDimensions { targetCoordinate = tc
+                         , listPromptSize = lps
+                         }
   where
     margin = 3
     padding = 3
+    tc = ((h `div` 2 - numChoices `div` 2) - padding, padding)
+    lps = (numChoices + margin, w - margin * 2)
 
 getDimensionsIO :: Int -> IO ListPromptDimensions
 getDimensionsIO n = getDimensions n <$> size
+
+withNoCursor = bracket_ hideCursor showCursor
+withNoEcho = bracket_ (hSetEcho stdin False) (hSetEcho stdin True)
+
+withNoBuffering handle newBuffering action = do
+    originalBuffering <- hGetBuffering handle
+    bracket_
+        (hSetBuffering handle newBuffering)
+        (hSetBuffering handle originalBuffering)
+        action
