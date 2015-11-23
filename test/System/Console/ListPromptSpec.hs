cd3b32e2e1eda7f852c7aff021949a87fa21ccde
yamadapc <tacla.yamada@gmail.com>
2015-11-23 18:17:26 -0200

    Fix test case

--- test/System/Console/ListPromptSpec.hs
+++ test/System/Console/ListPromptSpec.hs
@@ -1,16 +1,16 @@
 module System.Console.ListPromptSpec
   where
 
-import System.Console.ListPrompt.Internal
-import System.Console.ListPrompt.Types
+import           System.Console.ListPrompt.Internal
+import           System.Console.ListPrompt.Types
 
-import System.Console.Terminal.Size (Window(..))
-import Test.Hspec
+import           System.Console.Terminal.Size       (Window (..))
+import           Test.Hspec
 
 spec :: Spec
 spec = -- do
     describe "getDimensions" $ do
-        it "works when a `Window `is provided, centering the prompt" $ do
+        it "works when a `Window` is provided, centering the prompt" $ do
             let dim = getDimensions 10 (Just (Window 80 80))
 
             listPromptSize dim `shouldBe` (13, 74)
